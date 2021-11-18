424a40ae91e971834e2081b76def07f2ba83d698
Your Name <you@example.com>
2021-11-18 14:06:55 +0800

    game page update

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="game">
-    <h1>This is an game page</h1>
+  <div class="game bg-default">
+    <h1 class="text-light text-center text-hv-center text-sofia">Coming Soon...</h1>
   </div>
 </template>
 
@@ -11,3 +11,20 @@ export default {
   components: {}
 }
 </script>
+
+<style scoped>
+	.game{
+		position: relative;
+		height: 600px;
+	}
+
+	.text-hv-center{
+		margin: 0; 
+        position: absolute; 
+        top: 40%; 
+        left: 50%; 
+        transform: translate(-50%, -50%); 
+        padding: 15px 0;
+        text-shadow: 2px 2px 4px #000000;
+	}
+</style>
