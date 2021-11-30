711d52cc895d3357ad30bf3dc6ef0ca7fcda3f30
Your Name <you@example.com>
2021-11-30 15:07:27 +0800

    api url change

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import dotenv from 'dotenv'
 
 // import MoralisFactory from './moralis'
 import Moralis from 'moralis'
@@ -40,7 +41,7 @@ Vue.use(VueMeta)
 
 // Axios Definition
 const base = axios.create({
-	baseURL: process.env.BASE_URL
+	baseURL: process.env.API_URL
 });
 
 Vue.prototype.$http = base;
