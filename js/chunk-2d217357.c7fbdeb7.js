0ab6f3bb715b884c177ffb4d77b3f052d3fd6b65
Your Name <you@example.com>
2023-08-30 20:14:10 +0800

    Updates

---
+++ js/chunk-2d217357.c7fbdeb7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217357"],{c66d:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"profile"},[t("h1",[e._v("This is a profile page")])])}],i={name:"Profile",components:{}},l=i,r=t("2877"),a=Object(r["a"])(l,s,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d217357.c7fbdeb7.js.map
